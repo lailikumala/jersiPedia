b07d3578757c0faf6a065bcab6731dfeff637500
lailikumala <lailikumala92@gmail.com>
2022-08-22 14:39:39 +0700

    integration firebase at page jersey detail

--- src/pages/JerseyDetail/index.js
+++ src/pages/JerseyDetail/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, Alert} from 'react-native';
 import {
   colors,
   fonts,
@@ -7,6 +7,7 @@ import {
   responsiveHeight,
   heightMobileUI,
   responsiveWidth,
+  getData
 } from '../../utils';
 import {
   CardLiga,
@@ -17,11 +18,57 @@ import {
   Tombol,
 } from '../../components';
 import {RFValue} from 'react-native-responsive-fontsize';
+import {useDispatch, useSelector} from 'react-redux';
+import {getDetailLiga} from '../../config/actions/LigaAction';
+import {clearSaveKeranjang, masukKeranjang} from '../../config/actions/KeranjangAction';
 
 const JerseyDetail = ({navigation, route}) => {
-  const jersey = route.params.jersey;
-  const images = jersey.gambar
-  
+  const {jersey}              = route.params;
+  const images                = jersey.gambar
+  const dispatch              = useDispatch();
+  const [jumlah, setJumlah]   = useState('');
+  const [ukuran, setUkuran]   = useState('');
+  const [keterangan, setKet]  = useState('');
+  const {getDetailLigaResult} = useSelector(s => s.LigaReducer);
+  const {saveKeranjangLoading, 
+        saveKeranjangResult}  = useSelector(s => s.KeranjangReducer);
+
+  useEffect(() => {
+    dispatch(getDetailLiga(jersey.liga));
+  }, []);
+
+  useEffect(() => {
+    if (saveKeranjangResult) {
+      dispatch(clearSaveKeranjang());
+      navigation.navigate('Keranjang');
+    }
+  }, [saveKeranjangResult]);
+
+  const submitKeranjang = () => {
+    getData('user').then(res => {
+      if (res) {
+      //validasi form
+      if (jumlah && ukuran && keterangan && res.uid) {
+        //dispatch ke action masukKeranjang
+        dispatch(
+          masukKeranjang({
+            jersey: jersey,
+            ukuran: ukuran,
+            jumlah: jumlah,
+            keterangan: keterangan,
+            uid: res.uid,
+          }),
+        );
+      } else {
+        Alert.alert('Error', 'Jumlah, Ukuran & Keterangan harus diisi');
+      }
+    } else {
+      Alert.alert('Info', 'Silahkan Login Terlebih Dahulu');
+      navigation.replace('Login');
+    }
+    });
+  };
+      
     return (
       <View style={styles.page}>
         <View style={styles.button}>
@@ -31,10 +78,14 @@ const JerseyDetail = ({navigation, route}) => {
             onPress={() => navigation.goBack()}
           />
         </View>
-        <JerseySlider images={images} />
+        <JerseySlider images={jersey && jersey.gambar} />
         <View style={styles.container}>
           <View style={styles.liga}>
-            <CardLiga liga={jersey.liga} />
+            <CardLiga 
+            liga={getDetailLigaResult}
+            navigation={navigation}
+            id={jersey.liga}
+            />
           </View>
           <View style={styles.desc}>
             <Text style={styles.nama}>{jersey.nama}</Text>
@@ -55,6 +106,9 @@ const JerseyDetail = ({navigation, route}) => {
                 width={responsiveWidth(166)}
                 height={responsiveHeight(43)}
                 fontSize={13}
+                value={jumlah}
+                onChangeText={jumlah => setJumlah(jumlah)}
+                keyboardType="number-pad"
               />
               <Pilihan
                 label="Pilih Ukuran"
@@ -62,6 +116,8 @@ const JerseyDetail = ({navigation, route}) => {
                 height={responsiveHeight(43)}
                 fontSize={13}
                 datas={jersey.ukuran}
+                onValueChange={ukuran => setUkuran(ukuran)}
+                selectedValue={ukuran}
               />
             </View>
             <Inputan
@@ -69,6 +125,8 @@ const JerseyDetail = ({navigation, route}) => {
               textarea
               fontSize={13}
               placeholder="Isi jika ingin menambahkan Name Tag (nama & nomor punggung"
+              value={keterangan}
+              onChangeText={keterangan => setKet(keterangan)}
             />
             <Jarak height={15} />
             <Tombol 
@@ -77,6 +135,8 @@ const JerseyDetail = ({navigation, route}) => {
               icon="keranjang-putih"
               padding={responsiveHeight(17)}
               fontSize={18}
+              onPress={() => submitKeranjang()}
+              loading={saveKeranjangLoading}
             />
           </View>
         </View>
